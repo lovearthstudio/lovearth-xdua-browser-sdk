f5d658c4f9885278f2f36d531bff4c1a062d7553
焦帅 <lovearthcom@gmail.com>
2018-12-22 11:41:15 +0800

    去掉了oss中对demo代码的依赖,oss依赖放到oss里面

--- demo/option.js
+++ demo/option.js
@@ -1,11 +1,5 @@
 const APP_SECRET = 'e54b127abc7cca1862dac91db6256190'
 const APP_KEY = 'aHEVYhE0'
-const OSS_INFO = {
-    // url: 'http://115.29.50.216:3000',
-    url: 'http://api.xdua.com/sts/osser',
-    region: 'oss-cn-beijing',
-    bucket: 'file-xdua-com'
-}
 
 
 module.exports = {
